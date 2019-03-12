19e2dca4b7dbf77eea9482cba0c47d44fc594fd2
lishengjun <lishengjun@g7.com.cn>
2019-03-12 15:42:28 +0800

    TASK <PROJECT_NAME>-1234: Add nanohttpddemo and libnanohttpd modules SOLUTION: <brief description of what has been done>

---
+++ nanohttpddemo/src/main/java/org/nanohttpd/webserver/MainActivity.java
@@ -0,0 +1,15 @@
+package org.nanohttpd.webserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SimpleWebServer.creator(null);
+    }
+}
